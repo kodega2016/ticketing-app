4914eac0e5b19afa3bd0ea61b98476b3780c5310
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-10 05:43:33 +0545

    added sign out

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
+  req.session = null;
   res.status(200).json({
     data: null,
     message: "user is signed out successfully",
