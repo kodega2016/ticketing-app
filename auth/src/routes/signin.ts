08cb2178adf4fb95c5f48e0205f987556c101ade
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-09 19:00:06 +0545

    added sign in validation

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,12 +1,30 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.status(200).json({
-    data: null,
-    message: "user is logged in successfully",
-    success: true,
-  });
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    res.status(200).json({
+      data: null,
+      message: "user is logged in successfully",
+      success: true,
+    });
+  }
+);
 
 export { router as signinRouter };
