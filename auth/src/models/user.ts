2b8a891c6a3fd655099079d9fce56cb17eb843ad
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-09 18:48:20 +0545

    added transform to transform the data from mongo db to required json format

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -21,17 +21,29 @@ interface UserDoc extends mongoose.Document {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
