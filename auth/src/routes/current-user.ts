55a48b024340f8a4e7e9f65e9ad695a52f6d5579
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-10 07:28:46 +0545

    added current user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,33 +1,11 @@
 import express from "express";
-import { verify } from "jsonwebtoken";
-import { BadRequestError } from "../errors/bad-request-error";
-import { User } from "../models/user";
+import { currentUser } from "../middlewares/current-user";
 const router = express.Router();
 
-router.get("/api/users/currentuser", async (req, res) => {
-  if (!req.session?.jwt) {
-    return res.status(200).json({
-      data: null,
-      message: "current user is fetched successfully",
-      success: true,
-    });
-  }
-
-  const payload = verify(req.session.jwt, process.env.JWT_KEY!);
-  const { email, id } = payload as { email: string; id: string };
-  const user = await User.findById(id);
-
-  if (!user) {
-    throw new BadRequestError("User not found");
-  }
-
-  if (user.email !== email) {
-    throw new BadRequestError("Invalid credentials");
-  }
-
+router.get("/api/users/currentuser", currentUser, async (req, res) => {
   res.status(200).json({
     data: {
-      currentUser: user,
+      currentUser: req.currentUser || null,
     },
     message: "current user is fetched successfully",
     success: true,
