8e09ed19e6b5d320f1e4b7208fd0c09badc6a480
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-11 07:34:59 +0545

    wip[added initialProps

--- client/pages/banana.js
+++ client/pages/banana.js
@@ -1,5 +1,13 @@
-const Banana = () => {
+const Banana = ({ color }) => {
+  console.log("I was executed on the client", color);
   return <h1>Banana page</h1>;
+};
+
+Banana.getInitialProps = () => {
+  console.log("I was executed on the server");
+  return {
+    color: "yellow",
+  };
 };
 
 export default Banana;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,20 @@
-const Landing = () => {
+import axios from "axios";
+
+const Landing = ({ currentUser }) => {
+  console.log(currentUser);
+  console.log("I was executed on the client");
   return <h1>Landing Page</h1>;
 };
+
+Landing.getInitialProps = async () => {
+  if (typeof window === "undefined") {
+    console.log("I was executed on the server");
+  } else {
+    console.log("I was executed on the client");
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+  return {};
+};
+
 export default Landing;
