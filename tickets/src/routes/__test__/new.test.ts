ff5a02a1f433e3545d5821cbbe1f0047df930c16
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-15 20:18:38 +0545

    added ticket model and test cases for ticket creation

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,20 +1,19 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
-
   const response = await request(app)
-  .post("/api/tickets")
+    .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({});
 
   expect(response.status).not.toEqual(404);
 });
 
-
 it("returns an error if an invalid title is provided", async () => {
   await request(app)
-  .post("/api/tickets")
+    .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
       title: "",
@@ -22,18 +21,18 @@ it("returns an error if an invalid title is provided", async () => {
     })
     .expect(400);
 
-    await request(app)
+  await request(app)
     .post("/api/tickets")
-      .set("Cookie", global.signin())
-      .send({
-        price: 10,
-      })
-      .expect(400);
+    .set("Cookie", global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400);
 });
 
 it("returns an error if an invalid price is provided", async () => {
   await request(app)
-  .post("/api/tickets")
+    .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
       title: "title",
@@ -41,37 +40,33 @@ it("returns an error if an invalid price is provided", async () => {
     })
     .expect(400);
 
-    await request(app)
-    .post("/api/tickets")
-      .set("Cookie", global.signin())
-      .send({
-        title: "title",
-      })
-      .expect(400);
-});
-
-it("creates a ticket with valid inputs", async () => {
-  // add in a check to make sure a ticket was saved
-  // let tickets = await request(app)
-  // .get("/api/tickets")
-  //   .set("Cookie", global.signin())
-  //   .send({});
-
-  // expect(tickets.body.data.length).toEqual(0);
-
   await request(app)
-  .post("/api/tickets")
+    .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
       title: "title",
-      price: 10,
+    })
+    .expect(400);
+});
+
+it("creates a ticket with valid inputs", async () => {
+  const title = "flutter for mobile app development";
+  const price = 20;
+
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: title,
+      price: price,
     })
     .expect(201);
 
-    // tickets = await request(app)
-    // .get("/api/tickets")
-    //   .set("Cookie", global.signin())
-    //   .send({});
-
-    // expect(tickets.body.data.length).toEqual(1);
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(price);
 });
--- tickets/src/routes/__test__/new.ts
+++ tickets/src/routes/__test__/new.ts
@@ -1,18 +1,30 @@
-import { requireAuth, validateRequest } from '@kodetickets/common';
-import express, { Request, Response, Router } from 'express';
-import { body } from 'express-validator';
-const router:Router = express.Router();
+import { requireAuth, validateRequest } from "@kodetickets/common";
+import express, { Request, Response, Router } from "express";
+import { body } from "express-validator";
+import { Ticket } from "../../models/ticket";
+const router: Router = express.Router();
 
-router.post('/api/tickets',requireAuth,[
-  body('title').not().isEmpty().withMessage('Title is required'),
-  body('price').isFloat({ gt: 0 }).withMessage('Price must be greater than 0')
-],validateRequest, (req:Request, res:Response) => {
-  
-  res.status(201).send({
-    data: {},
-    message: 'ticket is created successfully',
-    success: true
-  })
-})
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
+    await ticket.save();
+
+    res.status(201).send({
+      data: ticket,
+      message: "ticket is created successfully",
+      success: true,
+    });
+  }
+);
 
 export { router as createTicketRouter };
