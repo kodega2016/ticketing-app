db5e7eac6178f55f9bc1476497f1e836e0d1668b
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-16 08:22:42 +0545

    added update ticket feature

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -43,3 +43,52 @@ it("return a 401 if the user does not own the ticket", async () => {
     })
     .expect(401);
 });
+
+it("return a 400 if the user provides an invalid title or price", async () => {
+  // create a ticket
+  const cookie = global.signin();
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "flutter and firebase for mobile app development",
+      price: 100,
+    });
+
+  // update the ticket
+  await request(app)
+    .put(`/api/tickets/${response.body.data.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "",
+      price: 100,
+    })
+    .expect(400);
+});
+
+it("updates the ticket provided valid inputs", async () => {
+  // create a ticket
+  const cookie = global.signin();
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "flutter and firebase for mobile app development",
+      price: 100,
+    });
+
+  // update the ticket
+  const title = "flutter and firebase for mobile app development";
+  const price = 200;
+  const updateResponse = await request(app)
+    .put(`/api/tickets/${response.body.data.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title,
+      price,
+    })
+    .expect(200);
+
+  expect(updateResponse.body.data.title).toEqual(title);
+  expect(updateResponse.body.data.price).toEqual(price);
+});
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -4,12 +4,19 @@ import {
   requireAuth,
 } from "@kodetickets/common";
 import { Request, Response, Router } from "express";
+import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
 const router = Router();
 
 router.put(
   "/api/tickets/:id",
   requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
     if (!ticket) {
