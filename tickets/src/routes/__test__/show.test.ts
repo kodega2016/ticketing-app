b5501ca10668f4c2765775795b633c95ea8d2eb1
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-16 07:47:30 +0545

    use mongoose new objectId for getting the objectID

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,11 +1,10 @@
+import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
-import { Ticket } from "../../models/ticket";
 
 it("returns a 404 if the ticket is not found", async () => {
-  const response = await request(app)
-    .get("/api/tickets/507f191e810c19729de860ea")
-    .send();
+  const id = new mongoose.Types.ObjectId().toHexString();
+  const response = await request(app).get(`/api/tickets/${id}`).send();
   expect(response.status).toEqual(404);
 });
 
