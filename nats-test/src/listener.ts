7e2d79aa13663931a305e1b76e1cb5dcd0f58ff4
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-20 22:43:06 +0545

    added parent class for listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,5 @@
 import { randomBytes } from "crypto";
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 
 console.clear();
 
@@ -41,3 +41,44 @@ stan.on("connect", () => {
 
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  protected ackWait = 5 * 1000;
+  private client: Stan;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setManualAckMode(true)
+      .setDeliverAllAvailable()
+      .setDurableName(this.queueGroupName)
+      .setAckWait(this.ackWait);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (msg: Message) => {
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf8"));
+  }
+}
