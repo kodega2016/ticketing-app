3a38c78c765f3842b47759e3c7acb094f55b48ce
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-20 22:57:55 +0545

    basic refactoring

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import { randomBytes } from "crypto";
-import nats, { Message, Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
@@ -10,64 +11,12 @@ const stan = nats.connect("ticketing", clientId, {
 
 stan.on("connect", () => {
   console.log("⚡️ listener connected to NATS ⚡️");
-
   stan.on("close", () => {
     console.log("NATS connection closed!");
     process.exit();
   });
-
   new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
 process.on("SIGTERM", () => stan.close());
-
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
-  protected ackWait = 5 * 1000;
-  private client: Stan;
-
-  constructor(client: Stan) {
-    this.client = client;
-  }
-
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setManualAckMode(true)
-      .setDeliverAllAvailable()
-      .setDurableName(this.queueGroupName)
-      .setAckWait(this.ackWait);
-  }
-
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
-
-    subscription.on("message", (msg: Message) => {
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  }
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-    return typeof data === "string"
-      ? JSON.parse(data)
-      : JSON.parse(data.toString("utf8"));
-  }
-}
-
-class TicketCreatedListener extends Listener {
-  subject: string = "ticket:created";
-  queueGroupName: string = "payments-service";
-  onMessage(data: any, msg: nats.Message): void {
-    console.log("Event data!", data);
-    msg.ack();
-  }
-}
