04263760b6141a6e57484ce8e3fc2b2ee8d347aa
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-23 23:21:55 +0545

    added add order functionality

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -7,9 +7,11 @@ import {
 import { Request, Response, Router } from "express";
 import { body } from "express-validator";
 import mongoose from "mongoose";
+import { Order, OrderStatus } from "../models/order";
 import { Ticket } from "../models/ticket";
 
 const router = Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   "/api/orders",
@@ -37,6 +39,25 @@ router.post(
     if (isReserved) {
       throw new BadRequestError("Ticket is already reserved");
     }
+    // calculate an expiration date for this order
+    // 15 minutes
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+    // build the order and save it to the database
+    // create the order
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+    // publish an event saying that an order was created
+    res.status(201).send({
+      data: order,
+      success: true,
+      message: "Order created successfully",
+    });
   }
 );
 
