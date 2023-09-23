7e0ab4080d66da2f62e720bf36171509bf33898c
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-23 18:04:05 +0545

    added validation for creating new order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,14 +1,28 @@
-import { requireAuth } from "@kodetickets/common";
-import { Router } from "express";
+import { requireAuth, validateRequest } from "@kodetickets/common";
+import { Request, Response, Router } from "express";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = Router();
 
-router.post("/api/orders", requireAuth, async (req, res) => {
-  res.status(200).send({
-    data: {},
-    message: "order is created successfully",
-    success: true,
-  });
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("ticketId is required"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.status(200).send({
+      data: {},
+      message: "order is created successfully",
+      success: true,
+    });
+  }
+);
 
 export { router as newOrderRouter };
