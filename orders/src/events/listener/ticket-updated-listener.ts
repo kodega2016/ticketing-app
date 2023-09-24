55e703e73f932132ae10f2e1afa7b453145f1744
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-24 22:54:34 +0545

    abstracted the query for getting the ticket by event

--- orders/src/events/listener/ticket-updated-listener.ts
+++ orders/src/events/listener/ticket-updated-listener.ts
@@ -10,11 +10,11 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
     data: TicketUpdatedEvent["data"],
     msg: Message
   ): Promise<void> {
-    const { id, title, price, version } = data;
-    const ticket = await Ticket.findOne({ _id: id, version: version - 1 });
+    const ticket = await Ticket.findByEvent(data);
     if (!ticket) {
       throw new Error("Ticket not found");
     }
+    const {  title, price } = data;
     ticket.set({ title, price });
     await ticket.save();
     msg.ack();
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -17,6 +17,7 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {id:string,version:number}): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -44,6 +45,16 @@ const ticketSchema = new mongoose.Schema(
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({ ...attrs, _id: attrs.id });
 };
+
+ticketSchema.statics.findByEvent = async (event: {
+  id: string;
+  version: number;
+})=>{
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+}
 
 ticketSchema.methods.isReserved = async function () {
   const existingOrder = await Order.findOne({
