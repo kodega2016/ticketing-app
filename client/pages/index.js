c71663045246db9ed8b52e8e08eb09136f8cdaeb
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-30 07:06:04 +0545

    fetch the list of ticket and show in the tickets page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,33 @@
-const LandingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>you are signed in</h1>
-  ) : (
-    <h1>you are not signed in</h1>
+const LandingPage = ({ currentUser, tickets: { data } }) => {
+  const ticketList = data.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
-LandingPage.getInitialProps = async (context,client,currentUser) => {
-  console.log("LANDING PAGE!");
-  console.log(currentUser);
-  return {};
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get("/api/tickets");
+  return {
+    tickets: data,
+  };
 };
 
 export default LandingPage;
--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,20 +1,66 @@
-const New = () => {
+import Router from "next/router";
+import { useState } from "react";
+import useRequest from "../../hooks/use-request";
+
+const NewTicket = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/tickets",
+    method: "post",
+    body: {
+      title,
+      price,
+    },
+    onSuccess: (data) => {
+      console.log(data);
+      Router.push("/");
+    },
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await doRequest();
+  };
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+    if (isNaN(value)) {
+      return;
+    }
+    setPrice(value.toFixed(2));
+  };
+
   return (
     <>
       <h1>Create a Ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" placeholder="Enter ticket name..." />
+          <input
+            value={title}
+            className="form-control"
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Enter ticket name..."
+          />
         </div>
         <div className="form-group">
           <label>Price</label>
-          <input className="form-control" placeholder="Enter ticket price..." />
+          <input
+            value={price}
+            onBlur={onBlur}
+            className="form-control"
+            onChange={(e) => setPrice(e.target.value)}
+            placeholder="Enter ticket price..."
+          />
         </div>
-        <button className="btn btn-primary mt-2">Submit</button>
+        {errors}
+        <button type="submit" className="btn btn-primary mt-2">
+          Submit
+        </button>
       </form>
     </>
   );
 };
 
-export default New;
+export default NewTicket;
