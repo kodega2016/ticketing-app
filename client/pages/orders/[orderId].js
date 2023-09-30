688acf529083d315954458b6537eee2105834e77
kodega2016 <khadgalovecoding2016@gmail.com>
2023-09-30 10:47:24 +0545

    added strip checkout for client app

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 import useRequest from "../../hooks/use-request";
+import StripeCheckout from "react-stripe-checkout";
+import Router from "next/router";
 
-const OrderShow = ({ order: { data } }) => {
+const OrderShow = ({ order: { data }, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   const { doRequest, errors } = useRequest({
     url: "/api/payments",
-    method: "get",
-    body: {},
-    onSuccess: (order) => {
-      // Router.push('/orders/[orderId]',  `/orders/${order.data.id}`);
+    method: "post",
+    body: {
+      orderId: data.id,
+    },
+    onSuccess: (payment) => {
+      console.log("payment successful", payment);
+      Router.push("/");
     },
   });
 
@@ -29,9 +34,19 @@ const OrderShow = ({ order: { data } }) => {
     return <div>Order Expired</div>;
   }
 
+  const onToken = async (token) => {
+    await doRequest({ token: token.id });
+  };
+
   return (
     <div>
       <h1>Order Show</h1>
+      <StripeCheckout
+        token={onToken}
+        amount={data.ticket.price * 100}
+        email={currentUser.email}
+        stripeKey="pk_test_51NvUDuG1jivoiAUMZ27YY6DebshIaXANkYbM8OlMhOC0YtFZdpZ512pcrDqMOAKYZZfaoUmISRLy8lGOEYLiEWGi00ctGq0wnD"
+      />
       <p>Time left to pay:{timeLeft} seconds</p>
     </div>
   );
--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -4,7 +4,9 @@ import { Ticket } from "../models/ticket";
 const router = Router();
 
 router.get("/api/tickets", async (req: Request, res: Response) => {
-  const tickets = await Ticket.find({});
+  const tickets = await Ticket.find({
+    orderId: undefined,
+  });
   res.status(200).send({
     data: tickets,
     message: "tickets are fetched successfully",
